97e101db7011da1deb24a06e0c5710ff480c17e9
emile kamana <emileizere@gmail.com>
2020-10-16 13:02:12 +0200

    order form edit

--- js/scripts.js
+++ js/scripts.js
@@ -205,3 +205,11 @@ $('.show-cart').on("change", ".item-count", function(event) {
 });
 
 displayCart();
+
+$("#deliver-check").click(function () {
+  if ($(this).is(":checked")) {
+      $("#deliver").show();
+  } else {
+      $("#deliver").hide();
+  }
+});
